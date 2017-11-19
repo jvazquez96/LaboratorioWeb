4635bd40cecfeea5b7e385ff86cc5887d18d1e3e
Irvel <irvel.nduva@gmail.com>
2017-11-19 17:04:56 -0600

    Add group frequency module PHP skeleton

---
+++ Sistema de Inscripciones/ModuloPHP/ReporteFrecuenciaGrupos.php
@@ -0,0 +1,26 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        #resultadosReporte {
+            padding-top: 10em;
+            padding-left: 6em;
+            padding-right: 6em;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        readfile("../web/header.html");
+    ?>
+
+    <p id="resultadosReporte">
+        asdfjjsdf
+    </p>
+</body>
+</html>
+
+
