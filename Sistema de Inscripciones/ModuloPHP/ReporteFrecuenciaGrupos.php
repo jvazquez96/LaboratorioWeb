a67f91fb8f66a7e7e15854e7b25953810b0281e4
Irvel <irvel.nduva@gmail.com>
2017-11-19 17:22:05 -0600

    Add connection to the mysql database

--- Sistema de Inscripciones/ModuloPHP/ReporteFrecuenciaGrupos.php
+++ Sistema de Inscripciones/ModuloPHP/ReporteFrecuenciaGrupos.php
@@ -1,3 +1,11 @@
+<?php
+$mysqli = new mysqli("localhost", "root", "", "Proyecto");
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
+
+?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
