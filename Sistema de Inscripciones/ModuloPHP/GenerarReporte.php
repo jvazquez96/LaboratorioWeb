e408a88ffbb3b4bd563adbbf42d57be58b110024
Irvel <irvel.nduva@gmail.com>
2017-11-19 19:26:40 -0600

    Calculate the total amount of times each classroom is used

--- Sistema de Inscripciones/ModuloPHP/GenerarReporte.php
+++ Sistema de Inscripciones/ModuloPHP/GenerarReporte.php
@@ -28,7 +28,27 @@ function salonesMasUtilizados() {
         return;
     }
 
-
+    // Contar la cantidad de cursos que utilizan cada salon
+    $repeticionesSalones = [];
+    while ($curso = $cursos->fetch_assoc()) {
+        $numeroSalon = $curso["Salon"];
+        if (array_key_exists($numeroSalon, $repeticionesSalones)) {
+            $repeticionesSalones[$numeroSalon] += 1;
+        }
+        else {
+            $repeticionesSalones[$numeroSalon] = 1;
+        }
+    }
+    $salonesConFrecuencia = ["table_head" => ["Numero Salon", "Capacidad", "Administrador",
+                             "Número de veces utilizado"]];
+    while ($salon = $salones->fetch_assoc()) {
+        $numeroSalon = $salon["Numero"];
+        $vecesUsado = (int)$repeticionesSalones[$numeroSalon];  // Nulls get turned into 0 by the magic of PHP
+        $salonesConFrecuencia["table_rows"][] = [$numeroSalon, $salon["Capacidad"],
+                                   $salon["Administrador"], $vecesUsado];
+    }
+    $salonesConFrecuencia["table_name"] = "Salones más utilizados";
+    echo json_encode($salonesConFrecuencia);
 }
 
 $tipoReporte = $_GET["tipoReporte"];
